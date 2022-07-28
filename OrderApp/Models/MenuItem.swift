7c30f36aaa56da9d8fe5cb038eae40a1b1cda579
Sergei Kovalev <skovalev1902@gmail.com>
2022-07-28 16:17:14 +0300

    r

---
+++ OrderApp/Models/MenuItem.swift
@@ -0,0 +1,29 @@
+//
+//  MenuItem.swift
+//  OrderApp
+//
+//  Created by Sergei Kovalev on 28.07.2022.
+//
+
+import Foundation
+
+struct MenuItems: Codable {
+    
+    var id: Int
+    var name: String
+    var detailText: String
+    var price: Double
+    var category: String
+    var imageURL: URL
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case datailText = "description"
+        case price
+        case category
+        case imageURL = "image_url"
+    }
+}
+
+
