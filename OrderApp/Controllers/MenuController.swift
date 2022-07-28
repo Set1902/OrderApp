3172c434bf6a2136cb4b3d53ede73739b80c15f1
Sergei Kovalev <skovalev1902@gmail.com>
2022-07-28 16:52:42 +0300

    r

--- OrderApp/Controllers/MenuController.swift
+++ OrderApp/Controllers/MenuController.swift
@@ -12,21 +12,47 @@ class MenuController {
     let baseURL = URL(string: "http://localhost:8080/")!
     
     
+    
+    
+    
+    
     func fetchCategories(completion: @escaping ((Result<[String], Error>) -> Void)) {
         let categoriesURL = baseURL.appendingPathComponent("categories")
+        
+        let task = URLSession.shared.dataTask(with: categoriesURL)
+        { (data, response, error) in
+            
+        }
+        task.resume()
     }
     
     func fetchMenuItems(forCategory categoryName: String, completion: @escaping ((Result<[MenuItem], Error>) -> Void)) {
         let baseMenuURL = baseURL.appendingPathComponent("menu")
         var components = URLComponents(url: baseMenuURL, resolvingAgainstBaseURL: true)
         components?.queryItems = [URLQueryItem(name: "category", value: categoryName)]
-        let menuUrl = components?.url!
+        let menuURL = components?.url!
+        
+        let task = URLSession.shared.dataTask(with: menuURL!)
+        { (data, response, error) in
+            
+        }
+        task.resume()
+        
     }
     
     typealias MinutesToPrepare = Int
     
     func submitOrder(forMenuIDs menuIDs: [Int], completion: @escaping ((Result<MinutesToPrepare, Error>) -> Void)) {
         let orderURL = baseURL.appendingPathComponent("order")
+        
+        var request = URLRequest(url: orderURL)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let data = ["menuIDs": menuIDs]
+        let jsonEncoder = JSONEncoder()
+        let jsonData = try? jsonEncoder.encode(data)
+        
     }
     
 }
