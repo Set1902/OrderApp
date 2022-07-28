766badb78c5c143084f061ce35f3735fd8c14b69
Sergei Kovalev <skovalev1902@gmail.com>
2022-07-28 17:19:12 +0300

    r

--- OrderApp/CategoryTableViewController.swift
+++ OrderApp/CategoryTableViewController.swift
@@ -8,15 +8,40 @@
 import UIKit
 
 class CategoryTableViewController: UITableViewController {
+    
+    let menuController = MenuController()
+    var categories = [String]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        menuController.fetchCategories { (result) in
+            switch result {
+            case .success(let categories):
+                self.updateUI(with: categories)
+            case .failure(let error):
+                self.displayError(error, title: "Failed to Fetch Categories")
+            }
+            
+        }
+        
+    }
+    
+    func updateUI(with categories: [String]) {
+        DispatchQueue.main.async {
+            self.categories = categories
+            self.tableView.reloadData()
+        }
+    }
+    
+    func displayError(_ error: Error, title: String) {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: title, message: error.localizedDescription, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
     }
 
     // MARK: - Table view data source
